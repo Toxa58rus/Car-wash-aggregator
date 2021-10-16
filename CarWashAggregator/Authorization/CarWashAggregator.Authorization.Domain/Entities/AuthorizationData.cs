9da8005719984fc42bb095bcddd2b8a1e1e308bb
Веревочников Дмитрий <vdv1998@yandex.ru>
2021-10-16 07:13:47 +0500

    AuthorizationManeger Added

--- CarWashAggregator/Authorization/CarWashAggregator.Authorization.Domain/Entities/AuthorizationData.cs
+++ CarWashAggregator/Authorization/CarWashAggregator.Authorization.Domain/Entities/AuthorizationData.cs
@@ -1,9 +1,12 @@
-﻿namespace CarWashAggregator.Authorization.Domain.Entities
+﻿using System;
+
+namespace CarWashAggregator.Authorization.Domain.Entities
 {
     public class AuthorizationData : BaseEntity
     {
         public string UserLogin { get; set; }
         public string HashPassword { get; set; }
         public string RefreshToken { get; set; }
+        public DateTime ExpireAt { get; set; }
     }
 }
